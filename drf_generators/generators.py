ac1c060922f38b6f163a20f8cd439069c2cab384
Tobin <tobinjbrown@gmail.com>
2015-04-07 13:08:36 -0500

    refactored generators to remove deprecated methods

--- drf_generators/generators.py
+++ drf_generators/generators.py
@@ -3,36 +3,30 @@ from django import template
 import os.path
 
 from drf_generators.templates import *
-from drf_generators.helpers import write_file
+from drf_generators.helpers import *
 
 __all__ = ['generate_serializers', 'generate_views', 'generate_urls']
 
 
 def generate_serializers(app_config):
-    app = app_config.models_module
-    name = name = app_config.name
+    name = app_config.name
     filename = 'serializers.py'
     message = 'Generating Serializers for %s (%s)' % (name, filename)
     print(message)
 
     temp = template.Template(SERIALIZER_FILE_TEMPLATE)
-    model_names = [m.__name__ for m in app_config.get_models()]
-    details = []
-    for m in app_config.get_models():
-        m_fields = m._meta.local_fields
-        fields = ', '.join(['\'%s\'' % x.name for x in m_fields])
-        detail = {'name': m._meta.object_name, 'fields': fields}
-        details.append(detail)
-
+    models = get_model_names(app_config)
+    details = get_model_details(app_config)
     context = template.Context({ 'app': name,
-                                 'models': model_names,
+                                 'models': models,
                                  'details': details})
 
     filename = 'serializers.py'
     content = temp.render(context)
+    app = app_config.models_module
     if write_file(content, filename, app):
-        for model in model_names:
-            print('  - %sSerializer' % model)
+        for serializer in get_serializer_names(models):
+            print('  - %s' % serializer)
     else:
         print('Serializer generation cancelled')
 
@@ -45,17 +39,16 @@ def generate_views(app_config):
     print(message)
 
     temp = template.Template(VIEW_FILE_TEMPLATE)
-    model_names = [m.__name__ for m in app_config.get_models()]
-    serializers = [x + 'Serializer' for x in model_names]
+    models = get_model_names(app_config)
+    serializers = get_serializer_names(models)
     context = template.Context({ 'app': name,
                                  'serializers': serializers,
-                                 'models': model_names })
+                                 'models': models })
 
     content = temp.render(context)
     if write_file(content, filename, app):
-        for model in model_names:
-            print('  - %sAPIView' % model)
-            print('  - %sAPIListView' % model)
+        for view in get_view_names(models):
+            print('  - %s' % view)
     else:
         print('View genereration cancelled')
 
@@ -68,9 +61,9 @@ def generate_urls(app_config):
     print(message)
 
     temp = template.Template(URL_FILE_TEMPLATE)
-    model_names = [m.__name__ for m in app_config.get_models()]
+    models = get_model_names(app_config)
     context = template.Context({ 'app': name,
-                                 'models': model_names })
+                                 'models': models })
 
     content = temp.render(context)
     if write_file(content, filename, app):
--- drf_generators/helpers.py
+++ drf_generators/helpers.py
@@ -1,5 +1,8 @@
 import os
 
+__all__ = ['write_file', 'get_model_names', 'get_model_details',
+           'get_serializer_names', 'get_view_names']
+
 
 def write_file(content, filename, app):
     name = os.path.join(os.path.dirname(app.__file__), filename)
@@ -12,3 +15,27 @@ def write_file(content, filename, app):
     new_file.write(content)
     new_file.close()
     return True
+
+
+def get_model_names(app_config):
+    return [m.__name__ for m in app_config.get_models()]
+
+
+def get_model_details(app_config):
+    details = []
+    for m in app_config.get_models():
+        m_fields = m._meta.local_fields
+        fields = ', '.join(['\'%s\'' % x.name for x in m_fields])
+        detail = {'name': m._meta.object_name, 'fields': fields}
+        details.append(detail)
+    return details
+
+
+def get_serializer_names(models):
+    return [m + 'Serializer' for m in models]
+
+
+def get_view_names(models):
+    api = [m + 'APIView' for m in models]
+    api_list = [m + 'APIListView' for m in models]
+    return api + api_list
